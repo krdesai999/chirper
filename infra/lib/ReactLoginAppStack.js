57b28d4fca5eba4aee275a92fccac1dfbe705286
krdesai999 <krdesai999@gmail.com>
2023-07-29 18:02:57 -0700

    s3 bucket deployment added

--- infra/lib/ReactLoginAppStack.js
+++ infra/lib/ReactLoginAppStack.js
@@ -1,5 +1,6 @@
 const { Stack, RemovalPolicy } = require('aws-cdk-lib');
 const { s3 } = require('aws-cdk-lib/aws-s3');
+const { s3Deploy } = require('aws-cdk-lib/aws-s3-deployment');
 
 class ReactLoginAppStack extends Stack {
   
@@ -16,6 +17,11 @@ class ReactLoginAppStack extends Stack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+    });
+
+    new s3Deploy.BucketDeployment(this, "DeployToBucket", {
+      destinationBucket: storageBucket,
+      sources: [s3Deploy.Source.asset("./resources/build")],
     });
   }
 }
