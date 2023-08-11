923ae9aa0c2d8372826241027a2474f8e2afe29d
krdesai999 <krdesai999@gmail.com>
2023-08-11 10:39:01 -0700

    Oauth token change

--- infra/config.js
+++ infra/config.js
@@ -1,7 +1,7 @@
 const githubConfig = {
   owner: "krdesai999",
   remoteURL: "https://github.com/krdesai999/chirper.git",
-  oauthToken: "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",
+  oauthToken: "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",
   repository: "chirper",
   productionBranch: "master",
   developmentBranch: "dev",
