6ad95bf70940c7f24db98a5a2bb1891035183d3f
krdesai999 <krdesai999@gmail.com>
2023-08-10 17:43:19 -0700

    aws amplify experiment

---
+++ infra/config.js
@@ -0,0 +1,11 @@
+const githubConfig = {
+  owner: "krdesai999",
+  remoteURL: "https://github.com/krdesai999/chirper.git",
+  oauthToken: "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",
+  repository: "chirper",
+  productionBranch: "master",
+  developmentBranch: "dev",
+  testBranch: "infra",
+};
+
+exports.githubConfig = githubConfig;
