2ad59e65bc819e74de6b12816b5065213bf25b97
krdesai999 <krdesai999@gmail.com>
2023-08-17 12:57:53 -0700

    header login/signup css added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,16 @@
+import "./Header.css";
+
 function Header() {
   return (
-      <div className="header">
-        header
+    <div className="header">
+      <div className="headerContent flex justify-between">
+        <div>Header</div>
+        <div className="flex my-1">
+          <div className="cursor-pointer btn">Login</div>
+          <div className="ml-1 mr-1 cursor-pointer btn">SignUp</div>
+        </div>
       </div>
+    </div>
   );
 }
 
