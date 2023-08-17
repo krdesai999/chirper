17d3b910eea97ec6c2b8cb21afb15a208ea7cf2f
krdesai999 <krdesai999@gmail.com>
2023-08-17 12:58:53 -0700

    changed name to home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Main from "./pages/main";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Home />
     </div>
   );
 }
