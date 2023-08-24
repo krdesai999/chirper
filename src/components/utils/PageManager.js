5cb4dff33265455fc49268f3ab1fdf62994ae0e5
krdesai999 <krdesai999@gmail.com>
2023-08-23 18:28:11 -0700

    pagemanager added

---
+++ src/components/utils/PageManager.js
@@ -0,0 +1,16 @@
+export const content = {
+  login: "login",
+  signUp: "signUp",
+  blogs: "blogs",
+};
+
+export var pageManager = {
+  loggedIn: false,
+  currentContent: content.login,
+};
+
+export const contentActions = {
+  tologin: "toLogin",
+  toSignUp: "toSignUp",
+  toBlogs: "toBlogs",
+};
