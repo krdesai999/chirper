6b28050e1932b090fe950055f1df87ca1238426b
krdesai999 <krdesai999@gmail.com>
2023-08-28 19:49:12 -0700

    React Router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blogs from './components/content/blogs';
+import LoginForm from './components/form/LoginForm';
 import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index path='/' element={<Home />}>
+      <Route index element={ <Blogs />} />
+      <Route path='login' element={<LoginForm />} />
+      <Route path="signUp" element={<LoginForm />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
