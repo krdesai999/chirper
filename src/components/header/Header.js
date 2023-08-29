a676fd866c370ba21d79c644f6d6ca5ccf6b5cd4
krdesai999 <krdesai999@gmail.com>
2023-08-28 20:26:30 -0700

    navlink and useAuth context switching

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,48 @@
-import { contentActions } from "../utils/PageManager";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../Auth";
 import "./Header.css";
 
-function Header( {contentManager, contentDispatch} ) {
+function Header({ contentManager, contentDispatch }) {
+  const auth = useAuth();
+
   return (
     <div className="header">
       <div className="headerContent flex justify-between">
         <div>Header</div>
-        <div className="flex my-1">
-          <div
-            className="cursor-pointer btn"
-            onClick={() => contentDispatch({
-              type: contentActions.tologin,
-            })}
-          >
-            Login
+
+        {!auth.user ? (
+          <div className="flex my-1">
+            <div
+              className="cursor-pointer btn"
+              onClick={() => NavLink("auth/login")}
+            >
+              Login
+            </div>
+            <div
+              className="ml-1 mr-1 cursor-pointer btn"
+              onClick={() => NavLink("auth/signUp")}
+            >
+              SignUp
+            </div>
           </div>
-          <div
-            className="ml-1 mr-1 cursor-pointer btn"
-            onClick={() => contentDispatch({
-              type: contentActions.toSignUp,
-            })}
-          >
-            SignUp
+        ) : (
+          <div className="profile-pic">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
