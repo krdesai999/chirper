d460999a23183cff6dd49684d961e39df5e8d479
krdesai999 <krdesai999@gmail.com>
2023-08-29 11:50:37 -0700

    Blog authentication redirect added

--- src/components/content/blogs.js
+++ src/components/content/blogs.js
@@ -1,7 +1,11 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../Auth";
+
 export default function Blogs() {
-    return (
-        <>
-        Blogs
-        </>
-    );
+    const auth = useAuth();
+    if (!auth.user) {
+      console.log("Login first!");
+      return <Navigate to="/auth/login" />;
+    }
+    return <div className="blogs">Blogs</div>;
 };
