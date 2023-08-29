2bc8b72eaf78ddfe49a9e20bcef717594a6180d3
krdesai999 <krdesai999@gmail.com>
2023-08-28 20:28:09 -0700

    Router updated for auth context and other routes added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth';
 import Blogs from './components/content/blogs';
 import LoginForm from './components/form/LoginForm';
+import AuthLayout from './layout/AuthLayout';
 import Home from "./pages/Home";
+import NotFound from './pages/NotFound';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route index path='/' element={<Home />}>
-      <Route index element={ <Blogs />} />
-      <Route path='login' element={<LoginForm />} />
-      <Route path="signUp" element={<LoginForm />} />
-    </Route>
+    <Auth>
+      <Route index path="/" element={<Home />}>
+        {/* Blogs pages */}
+        <Route index path="blogs" element={<Blogs />}></Route>
+        {/* Authentication pages */}
+        <Route path="auth" element={<AuthLayout />}>
+          <Route path="login" element={<LoginForm />} />
+          <Route path="signUp" element={<LoginForm />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Auth>
   )
 );
 
