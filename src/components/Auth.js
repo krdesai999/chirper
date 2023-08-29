e05f3252de640faabfe18193cca783d2a00f3c7b
krdesai999 <krdesai999@gmail.com>
2023-08-28 20:24:33 -0700

    Auth context created

---
+++ src/components/Auth.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export default function Auth({children}) {
+    const [user, setUser] = useState(null);
+
+    const login = (user) => {
+        setUser(user);
+    }
+
+    const logut = () => {
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, login, logut}} >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
